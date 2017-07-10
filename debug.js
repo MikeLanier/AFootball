63e82edc1ce21507316ab92abf604a2736877b78
Mike Lanier <Mike.Lanier1@comcast.net>
2017-07-10 08:45:06 -0500

    add controls to debug and react to keyup event

--- debug.js
+++ debug.js
@@ -1,11 +1,26 @@
 angular.module("AFootball")
-	.component("debug",{
+	// .component("debug",{
+	// 	templateUrl: "debug.html",
+	// 	bindings: { 
+	// 		name: "@",
+	// 		title: "=" 
+	// 	},
+	// 	controller: function() {
+	// 	  	this.myName = 'Mike';
+	// 	}
+	// })
+	.directive('debug', function() {
+    return {
 		templateUrl: "debug.html",
-		bindings: { 
-			name: "@",
-			title: "=" 
-		},
-		controller: function() {
-		  	this.myName = 'Mike';
-		}
-	});
+        link: function(scope, elem, attrs) {
+            elem.bind('keyup', function(event) {
+				console.log("checkKey: " + event.keyCode);
+                if (event.keyCode === 190) {
+					console.log("checkKey: period")
+				}
+                    return false;
+                // }
+            });
+        }
+    }
+});
