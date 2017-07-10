780946a2359f45be0910a7f26f96a806a3b9bc29
Mike Lanier <Mike.Lanier1@comcast.net>
2017-07-10 14:19:13 -0500

    output panel, stats panel, debug command parsing, change to gamedata in debug directive not passed back and/or not caught by scoreboard

--- debug.js
+++ debug.js
@@ -1,26 +1,87 @@
 angular.module("AFootball")
-	// .component("debug",{
-	// 	templateUrl: "debug.html",
-	// 	bindings: { 
-	// 		name: "@",
-	// 		title: "=" 
-	// 	},
-	// 	controller: function() {
-	// 	  	this.myName = 'Mike';
-	// 	}
-	// })
 	.directive('debug', function() {
     return {
 		templateUrl: "debug.html",
+		scope: { gamedata: '&'},
         link: function(scope, elem, attrs) {
+			scope.message = "";
             elem.bind('keyup', function(event) {
-				// console.log("checkKey: " + event.keyCode);
+
+				console.log("keyup: " + scope.message);
                 if (event.keyCode === 190) {
 					console.log("checkKey: period")
+	                // this.message = this.message.substr(0,i);
+	                // this.click();
+					scope.parse(scope.message);
+					scope.message = "";
 				}
-                    return false;
-                // }
-            });
+           });
+
+		   scope.parse = function(message) {
+				var items = message.split(',');
+				console.log("debug: click: " + message);
+				console.log("debug: items: " + items.length);
+				console.log("debug: items[0]: " + items[0]);
+				console.log("debug: items[1]: " + items[1]);
+				document.getElementById('junk').focus();
+
+				if(items[0] === 'd') {
+					console.log('debug: down: ' + items[1]);
+					scope.gamedata.down = parseInt(items[1]);
+					console.log('debug: down: ' + items[1] + ", " + scope.gamedata.down);
+				}
+				else
+				if(items[0] == 's') {
+					// console.log('debug: debug: score: ')
+					var who = 0;
+					if(items[1] == 'v') {
+						who = 1;
+					}
+
+					var val = parseInt(items[2]);
+					// console.log('debug: who, val: ' + who.toString() + ', ' + val.toString())
+
+					scope.gamedata.score[who][scope.gamedata.quarter] = val;
+					// console.log("debug: scope.gamedata.score[0]: " + scope.gamedata.score[0][0] + ", " + scope.gamedata.score[0][1] + ", " + scope.gamedata.score[0][2] + ", " + scope.gamedata.score[0][3] + ", " + scope.gamedata.score[0][4])
+					// console.log("debug: scope.gamedata.score[1]: " + scope.gamedata.score[1][0] + ", " + scope.gamedata.score[1][1] + ", " + scope.gamedata.score[1][2] + ", " + scope.gamedata.score[1][3] + ", " + scope.gamedata.score[0][4])
+
+					scope.gamedata.name[0] = "HOME" + val.toString()
+					scope.gamedata.name[1] = "VISITOR" + val.toString()
+
+				}
+				else
+				if(items[0] == 'q') {
+					scope.gamedata.quarter = parseInt(items[1]);
+				}
+				else
+				if(items[0] == 'y') {
+					console.log('debug:click:yardline');
+					scope.gamedata.yardline = parseInt(items[1]);
+				}
+				else
+				if(items[0] == 'g') {
+					scope.gamedata.togo = parseInt(items[1]);
+				}
+				else
+				if(items[0] == 't') {
+					scope.gamedata.time = parseInt(items[1]);
+				}
+				else
+				if(items[0] == 'o') {
+					// console.log('debug.offense: ' + items[1]);
+					if(items[1] == 'h' || items[1] == 'h.') {
+						// console.log('debug: offense: home')
+						scope.gamedata.offense = 0;
+						scope.gamedata.defense = 1;
+					}
+					else {
+						// console.log('debug: offense: visitor')
+						scope.gamedata.offense = 1;
+						scope.gamedata.defense = 0;                   
+					}
+					scope.gamedata.trigger++;
+				}
+			}
         }
     }
 });
--- stats.js
+++ stats.js
@@ -1,11 +1,48 @@
 angular.module("AFootball")
-	.component("stats",{
-		templateUrl: "stats.html",
-		bindings: { 
-			name: "@",
-			title: "=" 
-		},
-		controller: function() {
-		  	this.myName = 'Mike';
+	// .component("stats",{
+	// 	templateUrl: "stats.html",
+	// 	bindings: { 
+	// 	},
+	// 	controller: function() {
+	// 	  	this.myName = 'Mike';
+	// 	}
+	.directive('stats', function() {
+		return {
+			templateUrl: "stats.html",
+			link: function(scope, elem, attrs) {
+				scope.stats_title = { label: "--",	sublabel: "--",	home_value: "HOME",	visitor_value: "VIS" };
+				scope.stat_items = [
+					{ label: "Score",		sublabel: "",			home_value: "0",	visitor_value: "0"},
+					{ label: "First Downs",	sublabel: "",			home_value: "0",	visitor_value: "0"},
+					{ label: "Rush",		sublabel: "Yards",		home_value: "0",	visitor_value: "0"},
+					{ label: "",			sublabel: "Att",		home_value: "0",	visitor_value: "0"},
+					{ label: "",			sublabel: "TD",			home_value: "0",	visitor_value: "0"},
+					{ label: "Pass",		sublabel: "Yards",		home_value: "0",	visitor_value: "0"},
+					{ label: "",			sublabel: "Att",		home_value: "0",	visitor_value: "0"},
+					{ label: "",			sublabel: "Complete",	home_value: "0",	visitor_value: "0"},
+					{ label: "",			sublabel: "TD",			home_value: "0",	visitor_value: "0"},
+					{ label: "",			sublabel: "Int",		home_value: "0",	visitor_value: "0"},
+					{ label: "Total",		sublabel: "Yards",		home_value: "0",	visitor_value: "0"},
+					{ label: "",			sublabel: "Plays",		home_value: "0",	visitor_value: "0"},
+					{ label: "Fumb/Lost",	sublabel: "",			home_value: "0",	visitor_value: "0"},
+					{ label: "Punt",		sublabel: "Yards",		home_value: "0",	visitor_value: "0"},
+					{ label: "",			sublabel: "Att",		home_value: "0",	visitor_value: "0"},
+					{ label: "",			sublabel: "BK",			home_value: "0",	visitor_value: "0"},
+					{ label: "Punt",		sublabel: "Yards",		home_value: "0",	visitor_value: "0"},
+					{ label: "Return",		sublabel: "Att",		home_value: "0",	visitor_value: "0"},
+					{ label: "",			sublabel: "TD",			home_value: "0",	visitor_value: "0"},
+					{ label: "Kickoff",		sublabel: "Yards",		home_value: "0",	visitor_value: "0"},
+					{ label: "Return",		sublabel: "Att",		home_value: "0",	visitor_value: "0"},
+					{ label: "",			sublabel: "TD",			home_value: "0",	visitor_value: "0"},
+					{ label: "Int",			sublabel: "Yards",		home_value: "0",	visitor_value: "0"},
+					{ label: "Return",		sublabel: "Att",		home_value: "0",	visitor_value: "0"},
+					{ label: "",			sublabel: "TD",			home_value: "0",	visitor_value: "0"},
+					{ label: "FG",			sublabel: "Att",		home_value: "0",	visitor_value: "0"},
+					{ label: "",			sublabel: "Made",		home_value: "0",	visitor_value: "0"},
+					{ label: "",			sublabel: "Length",		home_value: "0",	visitor_value: "0"},
+					{ label: "Penalty",		sublabel: "Yards",		home_value: "0",	visitor_value: "0"},
+					{ label: "",			sublabel: "Num",		home_value: "0",	visitor_value: "0"}
+				];
+			}
 		}
 	});
