d4b519b0c64e5416b7b6373c5b7ebb750dbdd36d
Mike Lanier <Mike.Lanier1@comcast.net>
2017-07-12 16:40:05 -0500

    add mouseover and mouseleave functions

--- teamselectitem.js
+++ teamselectitem.js
@@ -6,6 +6,9 @@ angular.module("AFootball")
 		},
 		templateUrl: "teamselectitem.html",
         link: function(scope, elem, attrs) {
+
+			scope.hoverstyle={'border':'1px solid yellow'};
+
 			scope.closeNav = function() {
 				console.log("closeNav");
 		        document.getElementById("mySidenav").style.width = "0px";
@@ -13,6 +16,14 @@ angular.module("AFootball")
 
 			scope.click = function() {
 				console.log("click");
+			};
+
+			scope.mouseover = function() {
+				console.log("mouseover");
+			};
+
+			scope.mouseleave = function() {
+				console.log("mouseleave");
 			};
 		}
     }
