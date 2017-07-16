35a9e885e064000df42119474d8674efd635851a
Mike Lanier <Mike.Lanier1@comcast.net>
2017-07-16 08:22:09 -0500

    create directive for option button

---
+++ optionbutton.js
@@ -0,0 +1,31 @@
+angular.module("AFootball")
+	.directive('optionbutton', function() {
+    return {
+		scope: {
+			opttitle: '=',
+			xxx: '='
+		},
+		templateUrl: "optionbutton.html",
+        link: function(scope, elem, attrs) {
+
+			console.log("optionbutton: opttitle: " + scope.opttitle);
+			console.log("optionbutton: cmdID: " + scope.xxx);
+
+			scope.bgcolor = 'white-background';
+			
+			scope.mouseover = function() {
+				scope.bgcolor = 'yellow-background';
+				// console.log("mouseover");
+			};
+
+			scope.mouseleave = function() {
+				scope.bgcolor = 'white-background';
+				// console.log("mouseleave");
+			};
+
+			scope.click = function() {
+				console.log("click");
+			};
+		}
+    }
+});
