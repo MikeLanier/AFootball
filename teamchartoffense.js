e212b5bba89a3c1fc11b35a789358ad2dcebe05a
Mike Lanier <Mike.Lanier1@comcast.net>
2017-07-22 17:14:39 -0500

    team chart offense

--- teamchartoffense.js
+++ teamchartoffense.js
@@ -4,6 +4,40 @@ angular.module("AFootball")
 		scope: { gamedata: '=' },
 		templateUrl: "teamchartoffense.html",
         link: function(scope, elem, attrs) {
+
+			scope.results = [
+				{ lineno: 10 },
+				{ lineno: 11 },
+				{ lineno: 12 },
+				{ lineno: 13 },
+				{ lineno: 14 },
+				{ lineno: 15 },
+				{ lineno: 16 },
+				{ lineno: 17 },
+				{ lineno: 18 },
+				{ lineno: 19 },
+				{ lineno: 20 },
+				{ lineno: 21 },
+				{ lineno: 22 },
+				{ lineno: 23 },
+				{ lineno: 24 },
+				{ lineno: 25 },
+				{ lineno: 26 },
+				{ lineno: 27 },
+				{ lineno: 28 },
+				{ lineno: 29 },
+				{ lineno: 30 },
+				{ lineno: 31 },
+				{ lineno: 32 },
+				{ lineno: 33 },
+				{ lineno: 34 },
+				{ lineno: 35 },
+				{ lineno: 36 },
+				{ lineno: 37 },
+				{ lineno: 38 },
+				{ lineno: 39 },
+			];
+ 
 			scope.closeNav = function() {
 				// console.log("closeNav");
 		        document.getElementById("teamchartoffense").style.width = "0px";
